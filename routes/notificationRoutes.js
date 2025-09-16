bf223fdd106c2bb44cba3e17ef0efa1880cf4065
armandoxd03 <you@example.com>
2025-09-16 22:34:56 +0800

    notif

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const notificationController = require('../controllers/notificationController');
+const notificationController = require('../controllers/NotificationController');
 const auth = require('../middleware/auth');
 
 // Notifications
